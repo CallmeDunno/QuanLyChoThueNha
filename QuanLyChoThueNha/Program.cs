b8d8ed9066e2e9f6c3ac532423c19911b5fee7f8
Dunno <dungworkit@gmail.com>
2022-10-25 16:48:18 +0700

    test

--- QuanLyChoThueNha/Program.cs
+++ QuanLyChoThueNha/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyChoThueNha
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fMain());
+            Application.Run(new SplashScreen());
         }
     }
 }
