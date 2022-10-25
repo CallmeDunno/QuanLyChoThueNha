0271a767ebedb46922af3a2260be3f344cc43682
Dunno <dungworkit@gmail.com>
2022-10-25 23:47:11 +0700

    master

--- QuanLyChoThueNha/Program.cs
+++ QuanLyChoThueNha/Program.cs
@@ -16,6 +16,7 @@ namespace QuanLyChoThueNha
         {
             //dung
             //dung3
+            //edit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new fMain());
