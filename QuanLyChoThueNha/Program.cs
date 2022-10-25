3932fbec144fdb7115d8041ad9f61224826fbfd6
thuan-utc <thuansl99@gmail.com>
2022-10-25 22:29:43 +0700

    test

--- QuanLyChoThueNha/Program.cs
+++ QuanLyChoThueNha/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyChoThueNha
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fDanhMucNha());
+            Application.Run(new SplashScreen());
         }
     }
 }
