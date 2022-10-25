0a7304d91469aa4695f1054381b6f237ee6f8c62
CallmeDunno <88103030+CallmeDunno@users.noreply.github.com>
2022-10-25 23:27:46 +0700

    Update Program.cs

--- QuanLyChoThueNha/Program.cs
+++ QuanLyChoThueNha/Program.cs
@@ -14,6 +14,7 @@ namespace QuanLyChoThueNha
         [STAThread]
         static void Main()
         {
+            //edit
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new fMain());
