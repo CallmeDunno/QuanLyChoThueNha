96e5cbad47aacbf47be09d38b45052386fd5f8ba
thuan-utc <thuansl99@gmail.com>
2022-10-25 22:39:29 +0700

    resolve conflict

--- QuanLyChoThueNha/Program.cs
+++ QuanLyChoThueNha/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyChoThueNha
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SplashScreen());
+            Application.Run(new fMain());
         }
     }
 }
