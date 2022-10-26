b4874b7090a5e4ff2b567a5120aa9b83d66128bc
CallmeDunno <88103030+CallmeDunno@users.noreply.github.com>
2022-10-26 09:14:52 +0700

    Update Program.cs

--- QuanLyChoThueNha/Program.cs
+++ QuanLyChoThueNha/Program.cs
@@ -17,6 +17,7 @@ namespace QuanLyChoThueNha
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new fMain());
+            //deit
         }
     }
 }
